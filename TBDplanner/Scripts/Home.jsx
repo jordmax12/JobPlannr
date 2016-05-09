a4c58164030d80e7c0c4152cc69c5a44386bc0b8
Jordan Max <jordmax12@gmail.com>
2016-05-09 17:08:53 -0500

    ide commit

---
+++ TBDplanner/Scripts/Home.jsx
@@ -0,0 +1,13 @@
+﻿var Init = React.createClass({
+    render: function() {
+        return (
+          <div className="commentBox">
+            Hello, React!
+          </div>
+      );
+    }
+});
+ReactDOM.render(
+  <Init />,
+  document.getElementById('content')
+);
