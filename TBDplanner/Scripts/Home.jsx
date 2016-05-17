fd4f67f9c600d16cf70d365995df8ee95da50657
ethan-arrowood <ethan.arrowood@gmail.com>
2016-05-16 21:58:18 -0400

    updating

--- TBDplanner/Scripts/Home.jsx
+++ TBDplanner/Scripts/Home.jsx
@@ -1,13 +1,1 @@
-﻿var Init = React.createClass({
-    render: function() {
-        return (
-          <div className="commentBox">
-            Hello, React!
-          </div>
-      );
-    }
-});
-ReactDOM.render(
-  <Init />,
-  document.getElementById('content')
-);
+﻿
