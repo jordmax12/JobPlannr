1c65e9b3825baa562fa96287cf570cb9a32001a9
Jordan Max <jordmax12@gmail.com>
2016-10-27 21:59:37 -0500

    edit subtasks (not backend)

--- TBDplanner/Scripts/Components/Tasks.jsx
+++ TBDplanner/Scripts/Components/Tasks.jsx
@@ -16,6 +16,19 @@
         
     }
 
+    getIndexFromId(value, arr) {
+        console.log(arr);
+        for(var i = 0; i < arr.length; i++) {
+           
+            console.log(arr[i]);
+            console.log(value);
+            if(arr[i] === value) {
+                return i;
+            }
+        }
+        return -1; //to handle the case where the value doesn't exist
+    }
+
     getIndex(value, arr) {
         for(var i = 0; i < arr.length; i++) {
             if(arr[i] === value) {
@@ -55,10 +68,10 @@
 
             if(index != -1) {
                 //grab the task object
-                var taskObjectKey = this.state.tasks[index];  
+                var taskObject = this.state.tasks[index];  
 
                 //add new object to task object
-                taskObjectKey.SubTasks.push(subTaskObject);
+                taskObject.SubTasks.push(subTaskObject);
         
                 //update tasks
                 this.state.tasks[taskId - 1] = taskObjectKey;
@@ -134,47 +147,42 @@
         }
     }
 
-    EditSubTask(TaskId, SubTaskId) {
-        //need to write this logic now
-
+    EditSubTask(taskId, subTaskId) {
         var nextState = this.state;
-        //grab the task object, in case we need it
-        var indexedTask = this.state.editableTasks.filter(id => id === Id);
-        //check whether or not the task already exists
-        var alreadyExists = indexedTask.length > 0;
-        if(!alreadyExists) {
-            //add to array
-            nextState.editableTasks.push(Id);
-            this.setState(nextState);
-        } else {
-            var index = this.getIndex(indexedTask[0], this.state.editableTasks);
-            //index should never be -1, but just in case..
-            if(index != -1) {
-                var task = this.state.tasks.filter((_, i) => this.state.tasks[i].Id == Id)[0];
-                console.log(task);
-                //call subtask controller to add
-                $.ajax({
-                    type: "POST",
-                    url: "/Task/Update",
-                    data: JSON.stringify({id : Id, name : task.Name}),
-                    contentType: "application/json; charset=utf-8",
-                    success: function (data) {
-                        //return the new subtask id back here
-                        //this.AddSubTaskObject(taskId, data.ReturnObject.Id);
-                    }.bind(this),
-                    error: function (xhr, status, err) {
-                        console.log(err);
-                    }
-                });
+        //get the parent task key
+        var _task = this.state.tasks.filter((_, i) => this.state.tasks[i].Id == taskId)[0];
+        var index = this.getIndex(_task, this.state.tasks);
+
+        if(index != -1) {
+            //grab the task object
+            var taskObject = this.state.tasks[index];
+
+            //get the subtask key
+            var _subTask = taskObject.SubTasks.filter((_, i) => taskObject.SubTasks[i].Id == subTaskId)[0];
+            //get the subtask key
+            var subTaskIndex = this.getIndex(_subTask, taskObject.SubTasks);
+             //get the subtask object
+            var subTaskObject = taskObject.SubTasks[subTaskIndex];
+            //grab the task object, in case we need it
+            var indexedSubTask = this.state.editableSubTasks.filter(id => id === subTaskId);
+            //check whether or not the task already exists
+            var alreadyExists = indexedSubTask.length > 0;
+            if(!alreadyExists) {
+
+                //add to array
+                nextState.editableSubTasks.push(subTaskId);
+                this.setState(nextState);
+            } else {
+                console.log(this.state.editableSubTasks[0]);
+                console.log(this.state.editableSubTasks);
+                var subTaskIndexFromEditable = this.getIndexFromId(subTaskId, this.state.editableSubTasks);
+                console.log(subTaskIndexFromEditable)
                 //remove from array
                 this.setState({
-                    editableTasks: this.state.editableTasks.filter((_, i) => i !== index)
+                    editableSubTasks: this.state.editableSubTasks.filter((_, i) => i !== subTaskIndexFromEditable )
                 });
-                
             }
         }
-       
-
     }
 
     updateTask(id, name) {
@@ -247,18 +255,22 @@
     render() {
         const tasks = this.state.tasks.map((task, idx) => {
             var editable = this.state.editableTasks.filter(id => id === task.Id).length > 0;
+            var editableSubTasks = this.state.editableSubTasks;
             const subTaskComponents = task.SubTasks.map(subTask => 
+                //var subEditable = this.state.editableSubTasks.filter(id => id === subTask.Id).length > 0;
                 <li key={subTask.Id} className="list-group-item" style={{minHeight: '50px', border: 0, backgroundColor: 'rgba(127,191,63,.42)'}}>
                         <div className="pull-left" style={{width: '50%'}}>
-                            {subTask.Name}
+                            {editableSubTasks.filter(id => id === subTask.Id).length > 0 ? <input type="text" /> : <span>{subTask.Name}</span>}
                         </div>
-                       <div className="pull-right" style={{marginTop: '-5px', width: '50%'}}>
-                           <div className="pull-right">
-                                
-                                <button className="btn btn-default" onClick={() => { this.EditSubTask(task.Id, subTask.Id)}}>{editable ? <i className="fa fa-check"></i> : <i className="fa fa-pencil-square-o"></i>}</button>
-                           </div>
-                     </div>
+                        <div className="pull-right" style={{marginTop: '-5px', width: '50%'}}>
+                            <div className="pull-right">
+                                 <label>{subTask.Id}</label>
+                                 <button className="btn btn-default" onClick={() => { this.EditSubTask(task.Id, subTask.Id)}}>{editableSubTasks.filter(id => id === subTask.Id).length > 0  ? <i className="fa fa-check"></i> : <i className="fa fa-pencil-square-o"></i>}</button>
+                            </div>
+                        </div>
                 </li>
+            
+
             );
             
 
